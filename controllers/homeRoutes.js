47f6cc9727fe8eb297d6eb95066614c5e7f95112
Israel M. Rios <israelm.riosjr@gmail.com>
2021-11-20 15:37:35 -0800

    Added apis, models, public files, utils and views

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,42 @@
+const router = require("express").Router();
+const { Project, User } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", async (req, res) => {
+  try {
+  res.render('home', { logged_in: req.session.logged_in })
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/project/:id", async (req, res) => {
+  try {
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    res.render("all", {
+      layout: 'dash'
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+module.exports = router;
---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS coding_tech_db;
+CREATE DATABASE coding_tech_db;
